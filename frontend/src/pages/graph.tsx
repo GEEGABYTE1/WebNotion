95817f166087e3b8ba03a8ee41bf3cab36f90a1b
Jaival <pateljaival05@gmail.com>
2023-08-04 15:01:49 -0400

    dynamic graph render

--- frontend/src/pages/graph.tsx
+++ frontend/src/pages/graph.tsx
@@ -33,8 +33,83 @@ const testData = {
 export default function GraphViz() {
 
     const [apiData, setApiData] = useState()
+    const [graphDict, setGraphDict] = useState({})
+
+    const createidDict = (api_result) => {
+        const link_array = []
+        for (let i =0; i <= api_result.length; i++) {
+            const cur_relation = api_result[i]
+            if (cur_relation === undefined) {
+                continue 
+            } else {
+                const key = Object.keys(cur_relation)[0]
+                const val = Object.values(cur_relation)[0]
+                if (link_array.includes(key)) {} else {link_array.push(key)}
+                if (link_array.includes(val)) {} else {link_array.push(val)}
+            }
+        }
+
+        const link_id_dict = {}
+
+        for (let j =0; j <= link_array.length; j++) {
+            const cur_link = link_array[j]
+            if (cur_link === undefined) {
+                continue
+            } else {
+                link_id_dict[cur_link] = j
+            }
+        }
+
+        return link_id_dict
+    }
+
+    const createNodeArray = (link_dict) => {
+        var nodes = []
+        
+        for (let i =0; i<= Object.keys(link_dict).length; i++) {
+            const cur_id = Object.keys(link_dict)[i]
+            if (cur_id === undefined) {
+                continue
+            } else {
+                const link = link_dict[cur_id]
+                const temp_node_dict = {"id": link, "name": cur_id, "val": i}
+                nodes.push(temp_node_dict)
+            }
+        }
+
+        console.log(nodes)
+        return nodes
+    }
+
+
+    const createLinkArray = (link_dict, api_result) => {
+        const links = []
+        for (let api_idx = 0; api_idx <= api_result.length; api_idx++) {
+            const cur_relation = api_result[api_idx]
+            if (cur_relation === undefined) {
+                continue
+            } else {
+                const home_val = Object.keys(cur_relation)[0]
+                const landing_val = cur_relation[home_val]
+                const home_id = link_dict[home_val]
+                const landing_id = link_dict[landing_val]
+                links.push({"source": home_id, "target": landing_id})
+            }
+        }
+
+        console.log("links: ", links)
+        return links
+    }
+
+
+
 
     const handleSetApiData = (result) => {
+        const nodeArray = createNodeArray(createidDict(result))
+        const linkArray = createLinkArray(createidDict(result), result)
+        const graphData = {"nodes": nodeArray, "links": linkArray}
+        console.log("graph data: ", graphData)
+        setGraphDict(graphData)
         setApiData(result)
     }
 
@@ -54,7 +129,11 @@ export default function GraphViz() {
     return (
         <div>
             <Text>Graph</Text>
-            <ForceGraph3D graphData={testData} />
+            {Object.keys(graphDict) === 0?<div></div>:
+            
+            <ForceGraph3D graphData={graphDict} />
+            }
+            
             
         </div>
     )
